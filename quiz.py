2e6fdbc4aa7201e71be81c7ef18f33ae122ac001
Replit user <>
2023-05-02 14:51:46 +0000

    update to git

--- quiz.py
+++ quiz.py
@@ -49,12 +49,21 @@ global answer
 frame = list_frame[0]
 radio_buttons(tup1, frame)
 
+def calculate_score():
+    j = 0
+    list_answer = [100, 25, 576, 2, 20, 1, 28, 30, 45, 5]
+    for i in range(len(list_frame)):
+        if list_frame[i].winfo_ismapped() and store.get() == list_answer[i]:
+            j += 1
+    return j
+
 def forward(question_num):
     global next_button
     #global back_button
     global status_bar
     global frame
     global answer
+    global j
     if question_num <= len(list_tup):
         frame.grid_forget()
         frame = list_frame[question_num - 1]
@@ -65,13 +74,9 @@ def forward(question_num):
         status_bar.grid(row=2, column=0)
         next_button.grid(row=2, column=2, padx=10, pady=10)
         answer = store.get()
-        global j
-        j = 0
-        list_answer = [100, 25, 576, 2, 20, 1, 28, 30, 45, 5]
-        for i in range(len(list_frame)):
-            if list_frame[i] and answer == list_answer[i]:
-                j += 1
+        
     if question_num == len(list_tup) + 1:
+        j = calculate_score()
         frame.grid_forget()
         label1 = Label(root, text="You have " + str(j) + " points")
         label1.grid(row=1, column=1)
